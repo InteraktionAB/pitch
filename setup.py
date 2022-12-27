b2b88d1ebf7b2ec9f898c2c7292827810f87cbe2
Sreerag M <sreeragibtl@gmail.com>
2022-12-27 21:31:53 +0530

    dev: Add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pitch_extract',
+      version=' ',
+      description=' ',
+      author=' ',
+      author_email=' ',
+      url=' ',
+      packages=['distutils', 'distutils.command'],
+      py_modules=["pitch_extract"]
+     )
